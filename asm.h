bc921bd88c1f850f968e4b0020d4ce5394220a8f
Julian <j.sp@tuta.io>
2025-04-18 21:56:45 +0000

    implement more assembly

--- asm.h
+++ asm.h
@@ -4,21 +4,45 @@
     _start:\n"
 
 #define STACK_INIT_ASM                                                                                                 \
-    "sub rsp, 30000 \
+    "# STACK_INIT_ASM \
+    sub rsp, 30000 \
     mov rbx, rsp\n"
-#define STACK_CLEAN_ASM "add rsp, 30000\n"
+#define STACK_CLEAN_ASM                                                                                                \
+    "# STACK_CLEAN_ASM \
+    add rsp, 30000\n"
 
 #define EXIT_ASM                                                                                                       \
-    "mov rax, 60 \
+    "# EXIT_ASM \
+    mov rax, 60 \
     mov rdi, 0 \
     syscall\n"
 
-#define POINTER_INCREMENT_ASM "sub rbx, 1\n"
-#define POINTER_DECREMENT_ASM "add rbx, 1\n"
-#define CELL_INCREMENT_ASM "add [rbx], 1\n"
-#define CELL_DECREMENT_ASM "sub [rbx], 1\n"
-#define CELL_OUTPUT_ASM ""
-#define CELL_INPUT_ASM ""
+#define POINTER_INCREMENT_ASM                                                                                          \
+    "# POINTER_INCREMENT_ASM \
+    sub rbx, 1\n"
+
+#define POINTER_DECREMENT_ASM                                                                                          \
+    "# POINTER_DECREMENT_ASM \
+    add rbx, 1\n"
+#define CELL_INCREMENT_ASM                                                                                             \
+    "# CELL_INCREMENT_ASM \
+    add [rbx], 1\n"
+#define CELL_DECREMENT_ASM                                                                                             \
+    "# CELL_DECREMENT_ASM \
+    sub [rbx], 1\n"
+#define CELL_OUTPUT_ASM                                                                                                \
+    "# CELL_OUTPUT_ASM \
+    mov rax, 1 \
+    mov rdi, 1 \
+    mov rsi, rbx \
+    mov rdx, 1\n"
+#define CELL_INPUT_ASM                                                                                                 \
+    "# CELL_INPUT_ASM \
+    mov rax, 0 \
+    mov rdi, 1 \
+    mov rsi, rbx \
+    mov rdx, 1\n"
+
 #define JUMP_OPEN_ASM ".%s:\n"
 #define JUMP_CLOSE_ASM                                                                                                 \
     "cmp [rsp], 0 \
